c43fa7130a5278cd95fadb2c414ce8089cd44939
David Cuddihy <dtc78@cornell.edu>
2023-02-19 15:52:35 -0500

    fixing test to use unicode symbols

--- tests/test_view.py
+++ tests/test_view.py
@@ -2,6 +2,6 @@ import chess.view as view
 import chess.model as model
 
 def test_piece_to_char():
-    assert ' ' == view.piece_to_char(None)
-    assert 'P' == view.piece_to_char(model.Pawn(is_white=True))
-    assert 'p' == view.piece_to_char(model.Pawn(is_white=False))
+    assert '·' == view.piece_to_char(None)
+    assert '♙' == view.piece_to_char(model.Pawn(is_white=True))
+    assert '♟' == view.piece_to_char(model.Pawn(is_white=False))
