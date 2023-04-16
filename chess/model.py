bb1bb9ad45834f0fde62757d38a1d0984de1c759
Alex Albertsson <aalbertsson1@gmail.com>
2023-04-16 14:27:22 -0400

    added castle move and flag for if a piece has moved this game

--- chess/model.py
+++ chess/model.py
@@ -11,8 +11,10 @@ class Board:
     def get(self, location:str) -> Optional['Piece']:
         return self._squares.get(location, None)
 
-    def set(self, location:str, piece: 'Piece'):
+    def set(self, location:str, piece: 'Piece', setupFlag:bool = False):
         self._squares[location] = piece
+        if not setupFlag:
+            self._squares[location].setHasMoved()
 
     def removePiece(self, location:str):
         self._squares.pop(location)
@@ -29,12 +31,39 @@ class Board:
     def backupBoard(self):
         self._squares = self._lastState
 
+    def castleQueensideBlack(self):
+        self.set('d8', self.get('a8'))
+        self.set('c8', self.get('e8'))
+        self.removePiece('a8')
+        self.removePiece('e8')
+
+    def castleKingsideBlack(self):
+        self.set('f8', self.get('h8'))
+        self.set('g8', self.get('e8'))
+        self.removePiece('h8')
+        self.removePiece('e8')
+
+    def castleQueensideWhite(self):
+        self.set('d1', self.get('a1'))
+        self.set('c1', self.get('e1'))
+        self.removePiece('a1')
+        self.removePiece('e1')
+
+    def castleKingsideWhite(self):
+        self.set('f1', self.get('h1'))
+        self.set('g1', self.get('e1'))
+        self.removePiece('h1')
+        self.removePiece('e1')
+
+
+
 class Piece:
     """Abstract base class for chess pieces."""
     def __init__(self, is_white: bool, is_captured: bool, type:str) -> None:
         self._is_white = is_white
         self._is_captured = is_captured
-        self.type = type
+        self._type = type
+        self._has_moved = False
 
     def __hash__(self):
         return hash((type(self), self._is_white))
@@ -45,6 +74,16 @@ class Piece:
     def getPieceColor(self):
         return self._is_white
     
+    def getType(self):
+        return self._type
+
+    def setHasMoved(self):
+        self._has_moved = True
+
+    def getHasMoved(self):
+        return self._has_moved
+    
+
 
 class Pawn(Piece):
     def __init__(self, is_white: bool, is_captured: bool) -> None:
@@ -71,6 +110,8 @@ class Rook(Piece):
     def __init__(self, is_white: bool, is_captured: bool) -> None:
         super().__init__(is_white, is_captured, 'rook')
 
+
+
 class Game:
     """Class to handle mechanics of game, moving pieces, and validating user move commands"""
     def __init__(self):
@@ -82,6 +123,9 @@ class Game:
 
     def accept_move(self, move):
 
+        # make input lowercase to hanlde both cases
+        move = move.lower()
+
         # handle backup command
         if move == 'backup' and self.number_of_moves > 0 and self.has_backedup == False:
             self.board.backupBoard()
@@ -89,13 +133,47 @@ class Game:
             self.has_backedup = True
             self.white_to_play = not self.white_to_play
             return
-        
         if move == 'backup' and self.number_of_moves > 0 and self.has_backedup == True:
             print('Error: Can only back up one move')
             return
-        
         if move == 'backup' and self.number_of_moves == 0:
             return
+        
+        # handle castle move
+        # Kingside castle
+        if move == 'castlekingside' or move == 'castle kingside' or move == 'castleking' or move == 'castle king':
+            CastleValid = self.checkCastleValidity(self.white_to_play, 1)
+            # White kingside castle
+            if CastleValid and self.white_to_play:
+                self.board.castleKingsideWhite()
+                self.white_to_play = not self.white_to_play
+                return
+            # Black kingside castle
+            elif CastleValid and not self.white_to_play:
+                self.board.castleKingsideBlack()
+                self.white_to_play = not self.white_to_play
+                return
+            # Invalid castle conditions
+            elif not CastleValid:
+                print('Invalid castle move')
+                return
+        # Queenside castle
+        if move == 'castlequeenside' or move == 'castle queenside' or move == 'castlequeen' or move == 'castle queen':
+            CastleValid = self.checkCastleValidity(self.white_to_play, 0)
+            # White kingside castle
+            if CastleValid and self.white_to_play:
+                self.board.castleQueensideWhite()
+                self.white_to_play = not self.white_to_play
+                return
+            # Black kingside castle
+            elif CastleValid and not self.white_to_play:
+                self.board.castleQueensideBlack()
+                self.white_to_play = not self.white_to_play
+                return
+            # Invalid castle conditions
+            elif not CastleValid:
+                print('Invalid castle move')
+                return
 
         # source and destination of move
         source = move[0:2]
@@ -157,23 +235,23 @@ class Game:
     def set_up_pieces(self):
         """Place pieces on the board as per the initial setup."""
         for col in 'abcdefgh':
-            self.board.set(f'{col}2', Pawn(is_white=True,is_captured=False))
-            self.board.set(f'{col}7', Pawn(is_white=False,is_captured=False))
+            self.board.set(f'{col}2', Pawn(is_white=True,is_captured=False), True)
+            self.board.set(f'{col}7', Pawn(is_white=False,is_captured=False), True)
         for col in 'e':    
-            self.board.set(f'{col}1', King(is_white=True,is_captured=False))
-            self.board.set(f'{col}8', King(is_white=False,is_captured=False))
+            self.board.set(f'{col}1', King(is_white=True,is_captured=False), True)
+            self.board.set(f'{col}8', King(is_white=False,is_captured=False), True)
         for col in 'd':    
-            self.board.set(f'{col}1', Queen(is_white=True,is_captured=False))
-            self.board.set(f'{col}8', Queen(is_white=False,is_captured=False))
+            self.board.set(f'{col}1', Queen(is_white=True,is_captured=False), True)
+            self.board.set(f'{col}8', Queen(is_white=False,is_captured=False), True)
         for col in 'cf':    
-            self.board.set(f'{col}1', Bishop(is_white=True,is_captured=False))
-            self.board.set(f'{col}8', Bishop(is_white=False,is_captured=False))
+            self.board.set(f'{col}1', Bishop(is_white=True,is_captured=False), True)
+            self.board.set(f'{col}8', Bishop(is_white=False,is_captured=False), True)
         for col in 'bg':    
-            self.board.set(f'{col}1', Knight(is_white=True,is_captured=False))
-            self.board.set(f'{col}8', Knight(is_white=False,is_captured=False))        
+            self.board.set(f'{col}1', Knight(is_white=True,is_captured=False), True)
+            self.board.set(f'{col}8', Knight(is_white=False,is_captured=False), True)        
         for col in 'ah':    
-            self.board.set(f'{col}1', Rook(is_white=True,is_captured=False))
-            self.board.set(f'{col}8', Rook(is_white=False,is_captured=False))  
+            self.board.set(f'{col}1', Rook(is_white=True,is_captured=False), True)
+            self.board.set(f'{col}8', Rook(is_white=False,is_captured=False), True)  
 
         # save last state of board
         self.board.saveBoardState()
@@ -184,23 +262,66 @@ class Game:
 
         validMove = False
 
-        if piece.type == 'pawn':
+        if piece.getType() == 'pawn':
             validMove = True
 
-        elif piece.type == 'king':
+        elif piece.getType() == 'king':
             validMove = True
 
-        elif piece.type == 'queen':
+        elif piece.getType() == 'queen':
             validMove = True
 
-        elif piece.type == 'knight':
+        elif piece.getType() == 'knight':
             validMove = True
 
-        elif piece.type == 'bishop':
+        elif piece.getType() == 'bishop':
             validMove = True
 
-        elif piece.type == 'rook':
+        elif piece.getType() == 'rook':
             validMove = True
 
 
         return validMove
+    
+
+    def checkCastleValidity(self, whiteTurnFlag, sideFlag):
+        """check to see if the given castle move is valid"""
+        # parameters are whose turn it is (black = 0, white = 1) and which side to castle (queen = 0, king = 1)
+
+        # boolean for if castle move is valid
+        castleValid = False
+
+        # black queenside
+        if not whiteTurnFlag and not sideFlag:
+            # check that neither rook nor king have moved
+            if self.board.isOccupied('a8') and not self.board.get('a8').getHasMoved() and self.board.isOccupied('e8') and not self.board.get('e8').getHasMoved():
+                # check that spaces between rook and king are not occupied
+                if not self.board.isOccupied('b8') and not self.board.isOccupied('c8') and not self.board.isOccupied('d8'):
+                    castleValid = True
+
+        # black kingside
+        if not whiteTurnFlag and sideFlag:
+            # check that neither rook nor king have moved
+            if self.board.isOccupied('h8') and not self.board.get('h8').getHasMoved() and self.board.isOccupied('e8') and not self.board.get('e8').getHasMoved():
+                # check that spaces between rook and king are not occupied
+                if not self.board.isOccupied('f8') and not self.board.isOccupied('g8'):
+                    castleValid = True
+
+        # white queenside
+        if whiteTurnFlag and not sideFlag:
+            # check that neither rook nor king have moved
+            if self.board.isOccupied('a1') and not self.board.get('a1').getHasMoved() and self.board.isOccupied('e1') and not self.board.get('e1').getHasMoved():
+                # check that spaces between rook and king are not occupied
+                if not self.board.isOccupied('b1') and not self.board.isOccupied('c1') and not self.board.isOccupied('d1'):
+                    castleValid = True
+
+        # white kingside
+        if whiteTurnFlag and sideFlag:
+            # check that neither rook nor king have moved
+            if self.board.isOccupied('h1') and not self.board.get('h1').getHasMoved() and self.board.isOccupied('e1') and not self.board.get('e1').getHasMoved():
+                # check that spaces between rook and king are not occupied
+                if not self.board.isOccupied('f1') and not self.board.isOccupied('g1'):
+                    castleValid = True
+
+        # return result
+        return castleValid
